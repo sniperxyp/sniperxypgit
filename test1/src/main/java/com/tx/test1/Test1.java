4fd65324889fe3cef03324963dc4e33954712af9
sniperxyp <sniperxyp@163.com>
2016-03-08 17:42:55 +0800

    test1
    test1

---
+++ test1/src/main/java/com/tx/test1/Test1.java
@@ -0,0 +1,10 @@
+package com.tx.test1;
+
+/**
+ * Created by xiaoyipeng on 2016/3/8.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Here is the start of the programe");
+    }
+}
